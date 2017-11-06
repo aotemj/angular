13992a331cca67353b08b497a7a31b46906fde86
zhao <zxl@sina.com>
2017-11-06 17:04:22 +0800

    添加 增加数据,删除数据功能

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-(function (window) {
+(function (angular) {
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
-
-})(window);
+	var todoApp =angular.module("todoApp",[]);
+	todoApp.controller("todoController",["$scope",function($scope){
+		$scope.arr=[
+		{"name":"事件1","completed":false},
+		{"name":"事件2","completed":true},
+		{"name":"事件3","completed":false},
+		{"name":"事件4","completed":true},
+		{"name":"事件5","completed":true}
+		];
+		/*1.添加数据*/
+		$scope.add=function(){
+			$scope.arr.push({
+				"id":Math.random(),
+				"name":$scope.newItem,
+				"completed":false
+			});
+			/*添加完成后清空输入框*/
+			$scope.newItem="";
+		};
+/*2.删除数据*/
+	$scope.delete=function($index){
+		$scope.arr.splice($index,1);
+	}
+	}]);
+})(angular);
