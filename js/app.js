35616450021a0ff1d502ba29c114d5b0367c42f0
zhao <zxl@sina.com>
2017-11-06 19:48:15 +0800

    添加过滤显示数据功能,检查bug

--- js/app.js
+++ js/app.js
@@ -4,16 +4,10 @@
 	// Your starting point. Enjoy the ride!
 	var todoApp =angular.module("todoApp",[]);
 	todoApp.controller("todoController",["$scope",function($scope){
-		$scope.arr=[
-		{"name":"事件1","completed":false},
-		{"name":"事件2","completed":true},
-		{"name":"事件3","completed":false},
-		{"name":"事件4","completed":true},
-		{"name":"事件5","completed":true}
-		];
+		$scope.arr=[];
 		/*1.添加数据*/
 		$scope.add=function(){
-			if(!$scope.Item) return;
+			if(!$scope.newItem) return;
 			$scope.arr.push({
 				"id":Math.random(),
 				"name":$scope.newItem,
@@ -25,11 +19,11 @@
 /*2.删除数据*/
 	$scope.delete=function($index){
 		$scope.arr.splice($index,1);
-	}
+	};
 /*3.修改事件内容*/
 	$scope.change=function($index){
 		$scope.isEditingId=$index;
-	}
+	};
 	$scope.save=function(){
 		console.log("1");
 		$scope.isEditingId=-1;
@@ -40,5 +34,37 @@
 				$scope.arr[k].completed=$scope.checkAll;
 			}
 		};
-	}]);
+
+	/*5.添加修改事件的总数*/
+	$scope.getCount=function(){
+		var count=0;
+		for(var k in $scope.arr){
+			if($scope.arr[k].completed==false){
+				count++;
+			}
+		}
+		return count;
+	};
+
+	/*6.批量清除已完成事件*/
+	$scope.remove=function(){
+		for(var i =$scope.arr.length-1;i>=0;i--){
+			if($scope.arr[i].completed==true){
+				$scope.arr.splice(i,1);
+			}
+		}
+	};
+	/*7.过滤显示数据:*/
+	$scope.all=function(){
+			$scope.isCompleted={undefined};
+	};
+	$scope.active=function(){
+		$scope.isCompleted={"completed":false};
+	};
+	$scope.completed=function(){
+		$scope.isCompleted={"completed":true};
+	};
+	/*8.添加过滤显示数据样式:*/
+	// <a ng-class="{selected:isCompleted.completed==undefined}" href="#/" ng-click="all()">All</a>
+		}]);
 })(angular);
